6a427cdc93aabdf4caca54bd976bd175594c62e7
LJQ0 <ljq1019395070@163.com>
2024-07-02 14:46:25 +0800

    succsee

--- xiaomei.cpp
+++ xiaomei.cpp
@@ -5,7 +5,15 @@ using namespace std;
 int main() {
     int x, y;
     cin >> x >> y;
-    cout << 
-
-
+    int radius = ceil(sqrt(pow(x, 2) + pow(y, 2)));
+    if (radius == 0) {
+        cout << 10;
+    }
+    else if (radius > 0 && radius <= 10) {
+        cout << 11 - radius;
+    }
+    else {
+        cout << 0;
+    }
+    return 0;
 }
